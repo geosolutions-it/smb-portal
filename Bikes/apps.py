095960dd01b3c9113a5519a2091570c1f9135100
DarnelleF <yaemi.fevrier@gmail.com>
2018-05-11 14:48:12 -0400

    deleting and updating

--- Bikes/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BikesConfig(AppConfig):
-    name = 'Bikes'
--- Bikes/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Users
-
-
-class UserForm(ModelForm):
-    class Meta:
-        model =Users
-        exclude = []
