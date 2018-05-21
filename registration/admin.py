d27f4757f9b40bf2b0311e24eb981bfb0e0e0fdf
DarnelleF <yaemi.fevrier@gmail.com>
2018-05-21 07:11:50 -0400

    creating new groups

--- registration/admin.py
+++ registration/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 #from django.contrib.auth.models import User
 
-from .models import User,Profile,Vehicle,receipt,Tag, prize,VehicleType
+from .models import User,Vehicle,receipt,Tag, prize,VehicleType
 
 admin.site.register(VehicleType)
 #admin.site.register(EndUser)
-admin.site.register(Profile)
+#admin.site.register(Profile)
 admin.site.register(Vehicle)
 admin.site.register(receipt)
 admin.site.register(Tag)
