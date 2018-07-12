1afcf6a821b4739fe3b513e33fc85c82d2e3ab17
Giovanni Allegri <giohappy@gmail.com>
2018-07-12 12:53:24 +0200

    Move occupation field before phone (#88)

--- smbportal/profiles/forms.py
+++ smbportal/profiles/forms.py
@@ -40,8 +40,8 @@ class EndUserProfileForm(forms.ModelForm):
             "bio",
             "gender",
             "age",
-            "phone_number",
             "occupation",
+            "phone_number",
         )
         widgets = {
             "bio": forms.Textarea(
