b4d7f0d67d95947758b5d69de58bbdf9b2e439a5
giohappy <giohappy@gmail.com>
2018-07-24 18:48:50 +0200

    privacy policy

--- smbportal/profiles/models.py
+++ smbportal/profiles/models.py
@@ -13,6 +13,7 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
 from django.urls import reverse
+from django.utils.functional import lazy
 from django.utils.translation import gettext_lazy as _
 
 
@@ -37,7 +38,7 @@ class SmbUser(AbstractUser):
         default="it",
     )
     accepted_terms_of_service = models.BooleanField(
-        _("I accept the portal Terms of Service"),
+        _("I accept the portal <a href='/privacy_policy' target='_blank'>Privacy Policy</a>"),
         default=False
     )
 
