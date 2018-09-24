dfd9ca5ce44135066af8ffc76fca3be16c9c6098
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2018-09-24 17:38:24 +0100

    add badges fixes
    -  changed badge names to have an id for easing ordering
    -  added next_badges to user API endpoints

--- smbportal/badges/constants.py
+++ smbportal/badges/constants.py
@@ -8,56 +8,59 @@
 #
 #########################################################################
 
-from django.utils.translation import ugettext_lazy as _
+NEW_USER_BADGE = "01_new_user"
 
-# FIXME: Translation of these might be problematic
 # NOTE: Badge points are used for unlockables, which we are not using here
-CATEGORIES = {
-    "user_registration": {
-        "name": _("user_registration"),
-        "description": _(""),
+CATEGORIES = [
+    {
+        "name": "user_registration",
+        "description": "",
         "badge_definitions": [
             {
-                "name": _("new_user"),
-                "description": _(
+                "name": NEW_USER_BADGE,
+                "description": (
                     "As soon as you sign up and install the APP you earn "
                     "this badge."
-                ),
+                )
+                ,
                 "progression_target": 0,  # this badge is awarded immediately
             },
         ]
     },
-    "data_collection": {
-        "name": _("data_collection"),
-        "description": _(""),
+    {
+        "name": "data_collection_frequency",
+        "description": "",
         "badge_definitions": [
             {
-                "name": _("data_collector_level0"),
-                "description": _(
+                "name": "02_data_collector_level0",
+                "description": (
                     "When you start entering the tracking data of your "
                     "transport modes, you get this badge."
                 ),
                 "progression_target": 1,
+                "next": "03_data_collector_level1",
             },
             {
-                "name": _("data_collector_level1"),
-                "description": _(
+                "name": "03_data_collector_level1",
+                "description": (
                     "When you have recorded activity in a week for each day, "
                     "you will get this badge."
                 ),
                 "progression_target": 7,
+                "next": "04_data_collector_level2",
             },
             {
-                "name": _("data_collector_level2"),
-                "description": _(
+                "name": "04_data_collector_level2",
+                "description": (
                     "When you have recorded activity in two weeks for each "
                     "day, you will get this badge."
                 ),
                 "progression_target": 14,
+                "next": "05_data_collector_level3",
             },
             {
-                "name": _("data_collector_level3"),
-                "description": _(
+                "name": "05_data_collector_level3",
+                "description": (
                     "When you have recorded activity in a month for each day, "
                     "you will get this badge."
                 ),
@@ -66,55 +69,65 @@ CATEGORIES = {
 
         ]
     },
-    "bike_usage": {
-        "name": _("bike_usage"),
-        "description": _(""),
+    {
+        "name": "bike_usage_frequency",
+        "description": "",
         "badge_definitions": [
             {
-                "name": _("biker_level1"),
-                "description": _(
+                "name": "06_biker_level1",
+                "description": (
                     "Start using your bike in the city! Use the bike three "
                     "times in a week and you will get this badge."
                 ),
                 "progression_target": 3,
+                "next": "07_biker_level2",
             },
             {
-                "name": _("biker_level2"),
-                "description": _(
+                "name": "07_biker_level2",
+                "description": (
                     "Reuse the bike three more times in the city in the next "
                     "week and you will get this badge."
                 ),
                 "progression_target": 6,
+                "next": "08_biker_level3",
             },
             {
-                "name": _("biker_level3"),
-                "description": _(
+                "name": "08_biker_level3",
+                "description": (
                     "Reuse the bike in the city another six times in the "
                     "next two weeks and you will get this badge!!"
                 ),
                 "progression_target": 12,
             },
+        ]
+    },
+    {
+        "name": "bike_travel",
+        "description": "",
+        "badge_definitions": [
             {
-                "name": _("bike_surfer_level1"),
-                "description": _(
+                "name": "12_bike_surfer_level1",
+                "description": (
                     "Use the bike for at least 10 km in urban areas and you "
                     "will get this badge! You have made a great contribution "
                     "to sustainable mobility in your city!"
                 ),
                 "progression_target": 10,
+                "next": "13_bike_surfer_level2",
             },
             {
-                "name": _("bike_surfer_level2"),
-                "description": _(
+                "name": "13_bike_surfer_level2",
+                "description": (
                     "Use the bike for at least 50 km in urban areas and you "
                     "will get this badge! You have made a great contribution "
                     "to sustainable mobility in your city!"
                 ),
                 "progression_target": 50,
+                "next": "14_bike_surfer_level3",
             },
             {
-                "name": _("bike_surfer_level3"),
-                "description": _(
+                "name": "14_bike_surfer_level3",
+                "description": (
                     "Use the bike for at least 100 km in urban areas and "
                     "you will get this badge! You have made a great "
                     "contribution to sustainable mobility in your city!"
@@ -123,58 +136,68 @@ CATEGORIES = {
             },
         ]
     },
-    "public_transport_usage": {
-        "name": _("public_transport_usage"),
-        "description": _(""),
+    {
+        "name": "public_transport_usage_frequency",
+        "description": "",
         "badge_definitions": [
             {
-                "name": _("public_mobility_level1"),
-                "description": _(
+                "name": "09_public_mobility_level1",
+                "description": (
                     "Block your car and use urban public transport (tram, "
                     "bus, metro)! On first use of public transport you will "
                     "get this badge."
                 ),
                 "progression_target": 1,
+                "next": "10_public_mobility_level2",
             },
             {
-                "name": _("public_mobility_level2"),
-                "description": _(
+                "name": "10_public_mobility_level2",
+                "description": (
                     "Block your car and use urban public transport (tram, "
                     "bus, metro)! On the fifth use of public transport you "
                     "will get this badge."
                 ),
                 "progression_target": 5,
+                "next": "11_public_mobility_level3",
             },
             {
-                "name": _("public_mobility_level3"),
-                "description": _(
+                "name": "11_public_mobility_level3",
+                "description": (
                     "Block your car and use urban public transport (tram, "
                     "bus, metro)! On the tenth use of public transport you "
                     "will get this badge."
                 ),
                 "progression_target": 10,
-            },
+            }
+        ]
+    },
+    {
+        "name": "bus_travel",
+        "description": "",
+        "badge_definitions": [
             {
-                "name": _("tpl_surfer_level1"),
-                "description": _(
+                "name": "15_tpl_surfer_level1",
+                "description": (
                     "Use the bus for at least 25 km in urban areas and you "
                     "will get this badge! You have made a great contribution "
                     "to sustainable mobility in your city!"
                 ),
                 "progression_target": 25,
+                "next": "16_tpl_surfer_level2",
             },
             {
-                "name": _("tpl_surfer_level2"),
-                "description": _(
+                "name": "16_tpl_surfer_level2",
+                "description": (
                     "Use the bus for at least 100 km in urban areas and you "
                     "will get this badge! You have made a great contribution "
                     "to sustainable mobility in your city!"
                 ),
                 "progression_target": 100,
+                "next": "17_tpl_surfer_level3",
             },
             {
-                "name": _("tpl_surfer_level3"),
-                "description": _(
+                "name": "17_tpl_surfer_level3",
+                "description": (
                     "Use the bus for at least 200 km in urban areas and you "
                     "will get this badge! You have made a great contribution "
                     "to sustainable mobility in your city!"
@@ -183,56 +206,66 @@ CATEGORIES = {
             },
         ]
     },
-    "sustainability": {
-        "name": _("sustainability"),
-        "description": _(""),
+    {
+        "name": "sustainable_travel",
+        "description": "",
         "badge_definitions": [
             {
-                "name": _("multi_surfer_level1"),
-                "description": _(
+                "name": "18_multi_surfer_level1",
+                "description": (
                     "Use sustainable means for at least 100 km in urban "
                     "areas and you will get this badge! You have made a "
                     "great contribution to sustainable mobility in your city!"
                 ),
                 "progression_target": 100,
+                "next": "19_multi_surfer_level2",
             },
             {
-                "name": _("multi_surfer_level2"),
-                "description": _(
+                "name": "19_multi_surfer_level2",
+                "description": (
                     "Use sustainable means for at least 250 km in urban areas "
                     "and you will get this badge! You have made a great "
                     "contribution to sustainable mobility in your city!"
                 ),
                 "progression_target": 250,
+                "next": "20_multi_surfer_level3",
             },
             {
-                "name": _("multi_surfer_level3"),
-                "description": _(
+                "name": "20_multi_surfer_level3",
+                "description": (
                     "Use sustainable means for at least 500 km in urban areas "
                     "and you will get this badge! You have made a great "
                     "contribution to sustainable mobility in your city!"
                 ),
                 "progression_target": 500,
-            },
+            }
+        ]
+    },
+    {
+        "name": "avoid_co2",
+        "description": "",
+        "badge_definitions": [
             {
-                "name": _("ecologist_level1"),
-                "description": _(
+                "name": "21_ecologist_level1",
+                "description": (
                     "Avoid emissions for 25 kg of CO2 in urban areas and you "
                     "will get this badge!"
                 ),
                 "progression_target": 25,
+                "next": "22_ecologist_level2",
             },
             {
-                "name": _("ecologist_level2"),
-                "description": _(
+                "name": "22_ecologist_level2",
+                "description": (
                     "Avoid emissions for 50 kg of CO2 in urban areas and you "
                     "will get this badge!"
                 ),
                 "progression_target": 50,
+                "next": "23_ecologist_level3",
             },
             {
-                "name": _("ecologist_level3"),
-                "description": _(
+                "name": "23_ecologist_level3",
+                "description": (
                     "Avoid emissions for 100 kg of CO2 in urban areas and "
                     "you will get this badge!"
                 ),
@@ -241,30 +274,32 @@ CATEGORIES = {
 
         ]
     },
-    "health_benefits": {
-        "name": _("health_benefits"),
-        "description": _(""),
+    {
+        "name": "health_benefit",
+        "description": "",
         "badge_definitions": [
             {
-                "name": _("healthy_level1"),
-                "description": _(
+                "name": "24_healthy_level1",
+                "description": (
                     'Consume a total of 750 calories thanks to your movements '
                     '"active" in urban areas and you will get this badge!'
                 ),
                 "progression_target": 750,
+                "next": "25_healthy_level2",
             },
             {
-                "name": _("healthy_level2"),
-                "description": _(
+                "name": "25_healthy_level2",
+                "description": (
                     'Consume a total of 2250 calories thanks to your '
                     'movements "active" in urban areas and you will get this '
                     'badge!'
                 ),
                 "progression_target": 2250,
+                "next": "26_healthy_level3",
             },
             {
-                "name": _("healthy_level3"),
-                "description": _(
+                "name": "26_healthy_level3",
+                "description": (
                     'Consume a total of 4500 calories thanks to your '
                     'movements "active" in urban areas and you will get this '
                     'badge!'
@@ -273,29 +308,31 @@ CATEGORIES = {
             },
         ]
     },
-    "cost_savings": {
-        "name": _("cost_savings"),
-        "description": _(""),
+    {
+        "name": "cost_savings",
+        "description": "",
         "badge_definitions": [
             {
-                "name": _("money_saver_level1"),
-                "description": _(
+                "name": "27_money_saver_level1",
+                "description": (
                     "Save a total of 6 € thanks to your sustainable travel "
                     "in urban areas and you will get this badge!"
                 ),
                 "progression_target": 6,
+                "next": "28_money_saver_level2",
             },
             {
-                "name": _("money_saver_level2"),
-                "description": _(
+                "name": "28_money_saver_level2",
+                "description": (
                     "Save a total of 12 € thanks to your sustainable travel "
                     "in urban areas and you will get this badge!"
                 ),
                 "progression_target": 12,
+                "next": "29_money_saver_level3",
             },
             {
-                "name": _("money_saver_level3"),
-                "description": _(
+                "name": "29_money_saver_level3",
+                "description": (
                     "Save a total of 24 € thanks to your sustainable travel "
                     "in urban areas and you will get this badge!"
                 ),
@@ -303,5 +340,5 @@ CATEGORIES = {
             },
         ]
     },
-}
+]
 
--- smbportal/badges/management/commands/createbadgedefinitions.py
+++ smbportal/badges/management/commands/createbadgedefinitions.py
@@ -8,6 +8,8 @@
 #
 #########################################################################
 
+import re
+
 from django.core.management.base import BaseCommand
 
 import django_gamification.models as gm
@@ -15,7 +17,6 @@ import django_gamification.models as gm
 from badges import  utils
 from badges.constants import CATEGORIES
 import profiles.models as pm
-
 
 
 class Command(BaseCommand):
@@ -29,8 +30,8 @@ class Command(BaseCommand):
         self.stdout.write("Adding gamification interface to endusers...")
         self.add_user_gamification_interfaces()
         self.stdout.write("Generating badge definitions and badges...")
-        for cat_name, cat_config in CATEGORIES.items():
-            self.generate_category(**cat_config)
+        self.generate_categories(CATEGORIES)
+        self.generate_badge_definitions(CATEGORIES)
         self.stdout.write("Awarding `new user` badge for existing users...")
         self.award_new_user_badges()
         self.stdout.write("Done!")
@@ -46,22 +47,47 @@ class Command(BaseCommand):
             user = enduser_profile.user
             utils.add_gamification_interface(user)
 
-    def generate_category(self, name="", description="",
-                          badge_definitions=None, **kwargs):
-        category, created = gm.Category.objects.get_or_create(
-            name=name,
-            description=description
-        )
-        for definition in badge_definitions or []:
-            badge_def = gm.BadgeDefinition.objects.create(
-                name=definition["name"],
-                description=definition["description"],
-                points=definition.get("points"),
-                progression_target=definition.get("progression_target"),
-                category=category,
+    def generate_badge_definitions(self, category_definitions):
+        defs = []
+        for category_info in category_definitions:
+            for badge_info in category_info["badge_definitions"]:
+                info = badge_info.copy()
+                info["category"] = gm.Category.objects.get(
+                    name=category_info["name"])
+                defs.append(info)
+        ordered_defs = sort_badge_definitions(defs)
+        for badge_definition in reversed(ordered_defs):
+            next_name = badge_definition.get("next")
+            if next_name is not None:
+                next_badge = gm.BadgeDefinition.objects.get(name=next_name)
+            else:
+                next_badge = None
+            gm.BadgeDefinition.objects.create(
+                name=badge_definition["name"],
+                description=badge_definition["description"],
+                points=badge_definition.get("points"),
+                progression_target=badge_definition.get("progression_target"),
+                category=badge_definition["category"],
+                next_badge=next_badge
+            )
+
+    def generate_categories(self, category_definitions):
+        for category_info in category_definitions:
+            gm.Category.objects.get_or_create(
+                name=category_info["name"],
+                description=category_info["description"]
             )
 
     def award_new_user_badges(self):
         for enduser_profile in pm.EndUserProfile.objects.all():
             user = enduser_profile.user
             utils.award_new_user_badge(user)
+
+
+def sort_badge_definitions(items, regexp="(\d+)"):
+    return sorted(
+        items,
+        key=lambda item: [int(c) if c.isdigit() else c for
+                          c in re.split(regexp, item["name"])]
+    )
+
