5279a8cb0efc0c4d4318201af8fbc2b95b2f52de
GeoSolutions s.a.s <geosolutions@dev.savemybike.geo-solutions.it>
2019-08-05 16:27:20 +0000

    fixed GeoDjango OSM widget to account for mixed content

---
+++ smbportal/templates/gis/admin/osm.js
@@ -0,0 +1,10 @@
+{% extends "gis/admin/openlayers.js" %}
+{% block base_layer %}new OpenLayers.Layer.OSM("OpenStreetMap (SSL)",
+[
+   '//a.tile.openstreetmap.org/${z}/${x}/${y}.png',
+   '//b.tile.openstreetmap.org/${z}/${x}/${y}.png',
+   '//c.tile.openstreetmap.org/${z}/${x}/${y}.png'
+], 
+null
+);{% endblock %}
+
